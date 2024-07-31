9335c359d1366c334a64c7f00ea1ef4450903f5f
leesiyeon99 <qsqs9090@naver.com>
2024-07-31 12:02:05 +0900

    [Add] 리스트롸 링크드리스트_기본학습2

--- 리스트와 링크드리스트_기본학습2/Program.cs
+++ 리스트와 링크드리스트_기본학습2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace 리스트와_링크드리스트_기본학습2
+﻿using System.Collections.Generic;
+
+namespace 리스트와_링크드리스트_기본학습2
 /*과제 2. 요세푸스 순열
 아래의 요세푸스 순열에 대한 설명을 보고 N와 K 가 주어지면 결과를 출력하는 프로그램으로 구현하시오.
 
@@ -11,7 +13,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고 순서대로 K번째 사람을 제거하는 요세푸스 순열입니다.");
+            Console.Write("N번 숫자를 입력해주세요: ");
+            int.TryParse(Console.ReadLine(), out int n);
+            Console.Write("K번 숫자를 입력해주세요: ");
+            int.TryParse(Console.ReadLine(), out int k);
+
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                linkedList.AddLast(i);
+            }
+
+
+            while (linkedList.Count > 0)
+            {
+                for (int i = 1; i <= k; i++)
+                {
+                    LinkedListNode<int> node = linkedList.First;
+
+                    if (i == k)
+                    {
+                        Console.Write($"{node.Value},");
+                        linkedList.RemoveFirst() ;
+                    }
+                    else
+                    {
+                        linkedList.RemoveFirst();
+                        linkedList.AddLast(node);
+                    }
+                }
+
+            }
         }
     }
 }
+
